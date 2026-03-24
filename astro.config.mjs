06688f5052a0d4b761d3c11d28148ef4b4b6a61c
Damon Vinciguerra <damon.vinciguerra@cse-icon.com>
2026-03-24 06:57:46 -0500

    tailwinds? more like headwinds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://interlink.products.cse-icon.com',
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
